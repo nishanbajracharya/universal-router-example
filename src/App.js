5634ef9a5dbbb683f72a6318ff90209dfd1ef207
Nishan Bajracharya <nisbaj11@gmail.com>
2018-09-15 10:50:43 +0545

    Add global styles with styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { Provider } from 'react-redux';
 import React, { Component } from 'react';
+import { injectGlobal } from 'styled-components'
 
 import store from './store';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+`
 
 class App extends Component {
   render() {
