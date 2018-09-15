24f5e442f59e831eb1b84d715f671570663733dd
Nishan Bajracharya <nisbaj11@gmail.com>
2018-09-15 09:55:29 +0545

    Remove cra boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 import { Provider } from 'react-redux';
+import React, { Component } from 'react';
 
 import store from './store';
 
@@ -10,15 +7,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <p>Hello World</p>
       </Provider>
     );
   }
